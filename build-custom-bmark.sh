d20a82d0d4599094f49bc34bf8b20fa03c5b229b
Class Account <cs152-taf@eda-5.eecs.berkeley.edu>
2023-01-25 11:58:30 -0800

    scripted compilation and PATH stuff into subshell to minimize student headaches

--- build-custom-bmark.sh
+++ build-custom-bmark.sh
@@ -3,7 +3,11 @@
 bash << "EOF"
 source ./env.sh
 export PATH="/home/ff/cs152/sp22/lab1-install/bin:$PATH"
+<<<<<<< HEAD
 cd /scratch/${USER}/chipyard-cs152-sp24/generators/riscv-sodor/test/custom-bmarks
+=======
+cd /scratch/${USER}/chipyard-cs152-sp23/generators/riscv-sodor/test/custom-bmarks
+>>>>>>> 7f6aaf48 (scripted compilation and PATH stuff into subshell to minimize student headaches)
 make
 make run
 make dump
